5c1f0c813a722882701529ebeeec8103037d03b8
JP Mohr <James.Mohr2.0@hotmail.com>
2024-05-07 12:46:28 -0400

    Created class Car, added properties

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Corolla";
+            myCar.Year = 2007;
+
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
+            Console.WriteLine("They see me rollin', they hating!");
         }
     }
 }
